bab983925aa6b8800f358b69ab2cfe41904ba0e9
Rahul Deb <rahulbnc463@gmail.com>
2023-03-04 17:31:29 +0530

    try to create chat functionlity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles/app.css";
 import AllRoutes from "./Routes/AllRoutes";
+import Chat from "./Pages/Chat";
 
 const App = () => {
   return (
     <>
       <AllRoutes />
+      <Chat />
     </>
   );
 };
